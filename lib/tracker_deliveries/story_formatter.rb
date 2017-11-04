cc494c76b6f472b0e0b8949e5d6002511cf8a0e9
Jasonm23 <jasonm23@gmail.com>
2017-11-04 10:08:40 +0800

    Refactor story_url method added

--- lib/tracker_deliveries/story_formatter.rb
+++ lib/tracker_deliveries/story_formatter.rb
@@ -1,25 +1,28 @@
 module TrackerDeliveries
   class StoryFormatter
-    def initialize format, link
-      @link = link
+    def initialize format, story_link
+      @story_link = story_link
       @format = format
+    end
+
+    def wrap stories
+      wrapper stories
+                .map{|s| format_story s}
+                .join "\n"
+
+    end
+
+    def wrapper output
+      return %Q{<ul>\n#{output}\n</ul>} if @format == :html
+      output
+    end
+
+    def story_url s
+      @story_link + s.id
     end
 
     def format_story s
       return send(@format, s)
-    end
-
-    def wrap stories
-      wrap_output(
-        stories
-          .map{|s| format_story s}
-          .join "\n"
-      )
-    end
-
-    def wrap_output output
-      return %Q{<ul>\n#{output}\n</ul>} if @format == :html
-      output
     end
 
     def plaintext s
@@ -27,12 +30,12 @@ module TrackerDeliveries
     end
 
     def markdown s
-      link = %Q{[#{s.id}](#{@link + s.id})}
+      link = %Q{[#{s.id}](#{story_url s})}
       %Q{- #{link} - #{s.name}}
     end
 
     def html s
-      link = %Q{<a href="#{@link + s.id}">#{s.id}</a>}
+      link = %Q{<a href="#{story_url s}">#{s.id}</a>}
       %Q{<li>#{link} - #{s.name}</li>}
     end
   end
