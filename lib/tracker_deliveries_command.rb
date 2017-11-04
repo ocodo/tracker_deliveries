af7e42c6b386c949445991bbff642f38aef69955
Jasonm23 <jasonm23@gmail.com>
2017-11-04 14:15:28 +0800

    Clean up the command option handler

--- lib/tracker_deliveries_command.rb
+++ lib/tracker_deliveries_command.rb
@@ -5,16 +5,20 @@ class TrackerDeliveriesCommand
     @tracker_deliveries = tracker_deliveries
   end
 
-  def main format
-    if format == '--markdown'
-      puts @tracker_deliveries
-        .new(format: "markdown").delivered_stories
-    elsif format == '--html'
-      puts @tracker_deliveries
-        .new(format: "html").delivered_stories
-    else
-      puts @tracker_deliveries
-        .new().delivered_stories
-    end
+  def main args = nil
+    format = case args
+             when /--markdown/
+               "markdown"
+             when /--html/
+               "html"
+             when /--plaintext/
+               "plaintext"
+             else
+               "plaintext"
+             end
+
+    puts @tracker_deliveries
+      .new(format: format)
+      .delivered_stories
   end
 end
