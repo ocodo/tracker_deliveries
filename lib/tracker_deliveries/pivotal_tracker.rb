bab7c88aaeff732cffee42e0bcdb6fa754baa63a
Jasonm23 <jasonm23@gmail.com>
2017-11-04 09:40:48 +0800

    Refactor rename format_tools to format, improve code formatting

--- lib/tracker_deliveries/pivotal_tracker.rb
+++ lib/tracker_deliveries/pivotal_tracker.rb
@@ -10,28 +10,37 @@ module TrackerDeliveries
     def initialize project_id, api_key, options = {}
       @api_key = api_key
       @project_id = project_id
-      @format_tools = StoryFormatter.new(options[:format] || :plaintext, STORY_URL)
-      @api = Blanket.wrap API_URL,
-                          headers: { 'X-TrackerToken' => @api_key }
+
+      @formatter = StoryFormatter.new(
+        options[:format] || :plaintext,
+        STORY_URL
+      )
+
+      @api = Blanket.wrap(
+        API_URL,
+        headers: {
+          'X-TrackerToken' => @api_key
+        }
+      )
     end
 
     def delivered_stories
       options = {with_state: "delivered"}
-      wrap_output(api
-                    .projects(@project_id)
-                    .stories
-                    .get(params: options)
-                    .payload
-                    .map{|s| story_formatter s }
-                    .join("\n"))
+      wrap_output api
+        .projects(@project_id)
+        .stories
+        .get(params: options)
+        .payload
+        .map { |s| story_formatter s }
+        .join("\n")
     end
 
     def wrap_output output
-      @format_tools.wrap_output output
+      @formatter.wrap_output output
     end
 
     def story_formatter story
-      @format_tools.story_formatter story
+      @formatter.story_formatter story
     end
   end
 end
