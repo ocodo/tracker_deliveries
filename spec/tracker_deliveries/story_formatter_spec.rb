e65dc773a1ef97d3791f3ca6483d5775a0c68863
Jasonm23 <jasonm23@gmail.com>
2017-11-05 15:42:47 +0800

    Refactor formatter specs

--- spec/tracker_deliveries/story_formatter_spec.rb
+++ spec/tracker_deliveries/story_formatter_spec.rb
@@ -5,31 +5,53 @@ include TrackerDeliveries
 describe StoryFormatter do
   let(:format) { 'plaintext' }
   let(:url) { 'base_story_url' }
-  let(:formatter) {StoryFormatter
-                     .new(format, url)
-                     .get_formatter(format, url)}
+  let(:story_formatter) { StoryFormatter.new(format, url) }
+  let(:formatter) { story_formatter.get_formatter(format, url) }
+  let(:story_one) { OpenStruct.new({id: '123456', name: 'Story one'}) }
+  let(:story_two) { OpenStruct.new({id: '654321', name: 'Story two'}) }
+  let(:stories) { [story_one, story_two] }
 
-  context 'plaintext' do
+  context 'Formatters::Plaintext' do
     let(:format) { 'plaintext' }
 
     it 'selects the required formatter' do
       expect(formatter).to be_instance_of Formatters::Plaintext
     end
+
+    it 'formats stories' do
+      expect(story_formatter.wrap(stories))
+        .to eq "123456 - Story one\n" +
+               "654321 - Story two"
+    end
   end
 
-  context 'markdown' do
+  context 'Formatters::Markdown' do
     let(:format) { 'markdown' }
 
     it 'selects the required formatter' do
       expect(formatter).to be_instance_of Formatters::Markdown
     end
+
+    it 'formats stories' do
+      expect(story_formatter.wrap(stories))
+        .to eq "- [123456](base_story_url123456) - Story one\n" +
+               "- [654321](base_story_url654321) - Story two"
+    end
   end
 
-  context 'html' do
+  context 'Formatters::Html' do
     let(:format) { 'html' }
 
     it 'selects the required formatter' do
       expect(formatter).to be_instance_of Formatters::Html
+    end
+
+    it 'formats stories' do
+      expect(story_formatter.wrap(stories))
+        .to eq "<ul>\n" +
+               "<li><a href=\"base_story_url123456\">123456</a> - Story one</li>\n" +
+               "<li><a href=\"base_story_url654321\">654321</a> - Story two</li>\n" +
+               "</ul>"
     end
   end
 end
